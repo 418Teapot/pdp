a84dd89a14fece245aae8ac0e2ea90f222b38174
robotto <mark@moore.dk>
2015-05-06 13:29:38 +0200

    initial ping-pong test

--- nfcReader/src/nfcReader.ino
+++ nfcReader/src/nfcReader.ino
@@ -1,8 +1,13 @@
 
 void setup()
 {
+Serial.begin(9600);
 }
 
 void loop()
 {
+while(!Serial); //wait for serial port to open
+//Serial.setTimeout();
+while(!Serial.find("?")); //wait for ? from host
+Serial.println("!");
 }
