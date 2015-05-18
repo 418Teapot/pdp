e9a1e4dc5ab61f289c06d00f50d6b630bce81b02
Sebastian Skjold Højsted <sthazeb@gmail.com>
2015-05-18 12:13:48 +0200

    Added createUser files to the server!

--- add_veggie_frontend/createUser.php
+++ add_veggie_frontend/createUser.php
@@ -1,6 +1,15 @@
 <?
 if($_POST != null){ // post er ikke submitted!
-    echo "I was submitted!";
+    header("CHARSET=UTF-8");
+    if($_POST['password'] == $_POST['confPass']){
+    include_once("./database.php");
+        $password = saltPW($_POST['password']);
+    mysqli_query($db, "INSERT INTO users (username, password, email) VALUES('".$_POST['navn']."', '".$password."','".$_POST['email']."')") or die("Kunne ikke oprette brugeren. FEJL: ".mysqli_error($db));        
+        mysqli_close($db);
+        die("Brugeren er oprettet!");
+    } else {
+        die("Passwords var ikke ens - gå tilbage og prøv igen!");   
+    }
 } else {
 ?>
 <!doctype html>
@@ -10,15 +19,15 @@ if($_POST != null){ // post er ikke submitted!
         <meta charset="utf-8" />
     </head>
     <body>
-        <form method="post" action="<?=echo PHP_SELF; ?>">
+        <form method="post">
             <p>Brugernavn:
                 <input type="text" id="navn" name="navn" required /></p>
             <p>Password:
-                <input type="password" id="navn" name="navn" required /></p>
+                <input type="password" id="password" name="password" required /></p>
             <p>Gentag Password:
-                <input type="password" id="navn" name="navn" required /></p>
+                <input type="password" id="confPass" name="confPass" required /></p>
             <p>Email:
-                <input type="email" id="navn" name="navn" required /></p>
+                <input type="email" id="email" name="email" required /></p>
             <button type="submit">Opret bruger!</button>            
         </form>
     </body>
--- add_veggie_frontend/database.php
+++ add_veggie_frontend/database.php
@@ -18,4 +18,10 @@ if(mysqli_connect_errno()){
     echo "Fejl: " . mysqli_connect_error();
 }
 
+// password salting function
+function saltPW($pw){
+        $salt = md5("abcdef");
+        return md5($salt.$pw.$salt);     
+}
+
 ?>
